ab5534ae673bbaed713f6228b853274edb20bae2
Max Skvortsov <mkmax1615@gmail.com>
2018-07-25 21:45:56 +0300

    Add main js file

--- soap/task1/index.php
+++ soap/task1/index.php
@@ -1,51 +1,31 @@
 <?php
 
-function getCurrenciesCURL(){
-        $url
-= "/ListOfCurrenciesByCode";
-    
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-    $response = curl_exec($ch);
-    $xml = new SimpleXMLElement($response);
-    return $xml->tCurrency;
+include 'config/config.php';
+spl_autoload_register(function ($class_name) {
+    include 'libs/'.$class_name.'.php';
+});
+
+$action = $_POST['action'];
+list($class, $method, $param) = explode($action);
+if($class == "soap"){
+    $class = new SoapCl();
+} elseif($class == "curl"){
+    $class = new CurlCl();
 }
 
-function invertStrCURL($string){
-    $xml = '<?xml version="1.0" encoding="utf-8"?>
-            <soap12:Envelope xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-                <soap12:Body>
-                    <InvertStringCase xmlns="http://www.dataaccess.com/webservicesserver/">
-                        <sAString>$string</sAString>
-                    </InvertStringCase>
-                </soap12:Body>
-            </soap12:Envelope>';
-    $url = TEXT_CASING_SERVICE.;
-    $headers = array(
-        'POST /webservicesserver/TextCasing.wso HTTP/1.1 ',
-        'HOST: www.dataaccess.com',
-        'Content-Type: application/soap+xml; charset="utf-8"',
-        'Content-Length: '.strlen($xml),
-    );
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml");
-    $response = curl_exec($ch);
-    return $response;
+if($param){
+    $class->$method($param);
+} else {
+    $class->$method();
 }
 
+
+include("templates/index.php");
 //$result = invertStrCURL();
-$result = getCurrenciesCURL();
-foreach($result as $obj){
-    var_dump($obj->sISOCode);
-}
+//$result = getCurrenciesCURL();
+//foreach($result as $obj){
+    //var_dump($obj->sISOCode);
+//}
 
 
 /* SOAP
--- soap/task1/templates/index.php
+++ soap/task1/templates/index.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>SOAP and cURL Clients</title>
-    </head>
-    <body>
-        
-    </body>
+<head>
+    <title>SOAP and cURL</title>
+    <script
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <div>
+        <h2><span>Enter a text for invert text casing</span></h2><br>
+        <label for="text-casing-inp">Enter a text</label>
+        <input type="text" name="text-casing-inp"><br>
+        <br>
+        <input type="button" name="soap-butt-1" value="Invert (SOAP)">
+        <input type="button" name="curl-butt-1" value="Invert (cURL)">
+        <div id="result-1"></div>
+    </div>
+    <div>
+        <h2><span>Press the button to get list of currencies</span></h2><br>
+        <input type="button" name="soap-butt-2" value="Get currencies (SOAP)">
+        <input type="button" name="curl-butt-2" value="Get currencies (cURL)">
+        <div id="result-2"></div>
+    </div>
+</body>
 </html>
+
