be458327554fba8b44971dda08294370c85498c9
Max Skvortsov <mkmax1615@gmail.com>
2018-08-14 16:01:53 +0300

    Add files via upload

--- js-calendar/js/main.js
+++ js-calendar/js/main.js
@@ -11,46 +11,55 @@ let app = new Vue({
 			'Sun'
 		],
 		months: [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
+			'January',
+			'Febuary',
+			'March',
+			'April',
 			'May',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Oct',
-			'Now',
-			'Dec'
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'Nowember',
+			'December'
 		],
 		monthArr: [
 
 		],
-		/*selectedData: {
+		selectedDate: {
 			year: null,
-			month: null,
-			day: null
-		}*/
+			month: null
+		}
 	},
 	methods: {
-		buildCalendar: function(year, month){
+		buildCalendar: function(yearInp=null, monthInp=null){
+			this.setSelectedDate(yearInp, monthInp);
+
 			//let currDate = new Date();
-			let currYear = new Date().getFullYear();
+			/*let currYear = new Date().getFullYear();
 			let currMonth = new Date().getMonth();
-			let currDay = new Date()
+			let currDay = new Date();
+			*/
+			console.log(this.selectedDate.year);
+			console.log(this.selectedDate.month);
+
+			let year = this.selectedDate.year;
+			let month = this.selectedDate.month;
+
 			let daysInMonth = this.daysInMonth(year, month);
 			let firstMonthDay = this.firstMonthDay(year, month);
-			let colsTotal = Math.round((daysInMonth+firstMonthDay)/7)*7; 
-			console.log(`firstMonthDay:${this.firstMonthDay(year, month)}`);
-			console.log(`month:${month}`);
-			console.log(`daysInMonth: ${this.daysInMonth(year, month)}`);
-			console.log(`daysInMonth: ${colsTotal}`);
+			let colsTotal = Math.ceil((daysInMonth+firstMonthDay)/7)*7; 
+
+			console.log(`firstMonthDay:${firstMonthDay}`);
+			//console.log(`month:${month}`);
+			console.log(`daysInMonth: ${daysInMonth}`);
+			console.log(`colsTotal: ${colsTotal}`);
 			//console.log(`currWeekDay:${this.currWeekDay(year, month)}`);
 			let colIndex = 0;
 			let day = 1;
-			//let monthArr = [];
 			let week = 1;
+			this.monthArr = [];
 			this.monthArr[week] ={};
 			do{
 				let currDayNum = colIndex%7;
@@ -61,50 +70,92 @@ let app = new Vue({
 			    	//monthArr[week][colIndex%7] = {};
 			    } else {
 			    	className = '';
-			    	if(currYear == year && 
-			    		currMonth == month &&
-			    		currDay == day){
+			    	if(this.currYear() == year && 
+			    		this.currMonth() == month &&
+			    		this.currDay() == day){
 			    		className = 'today';
 			    	} 
 			    	else if (currDayNum == 6 || currDayNum == 5) {
 			    		className = 'holiday';
 			    	}
-			    	this.monthArr[week][currDayNum] = {'year': year, 'month':month, 'day': day, 'class': className};
+			    	this.monthArr[week][currDayNum] = {
+			    		'year': year, 
+			    		'month': month, 
+			    		'day': day, 
+			    		'class': className
+			    	};
 			    	day++
 			    }
 			    if(currDayNum == 6){
 			    	
 			    	week++;
 			    	if (week <= (colsTotal/7)) {
-			    		this.monthArr[week] ={};
+			    		this.monthArr[week] = {};
 			    	}    	
 			    }
 			    colIndex++;
 			    
-			}while(colIndex < colsTotal);
+			} while(colIndex < colsTotal);
 			
 		},
 		daysInMonth: function(year, month){
-			return new Date(year, month-1, 0).getDate();	
+			return new Date(year, month+1, 0).getDate();	
 		},
 		currDay: function(year, month){
-			return new Date(year, month-1).getDate();
+			return new Date(year, month).getDate();
 		},
-		/*currWeekDay: function(year, month){
-			let res = new Date(year, month-1).getDay();
-			
-			return res;
-		},*/
+		currYear: function(){
+			return new Date().getFullYear();
+		},
+		currMonth: function(){
+			return new Date().getMonth();
+		},
 		firstMonthDay: function(year, month){
 
-			let res = new Date(year, month-1, 1).getDay();
+			let res = new Date(year, month, 1).getDay();
 			if (res == 0) {
-				return 7;
+				res = 7;
 			}
-			return res;
-		} 
+			return --res;
+		},
+		setSelectedDate: function(year, month) {
+			if (month !== null) {
+				if (month > 11) {
+					month = 0;
+					this.selectedDate.year++;
+				} else if (month < 0) {
+					month = 11;
+					this.selectedDate.year--;
+				};
+				this.selectedDate.month = month;
+			} else {
+				if (this.selectedDate.month === null) {
+					this.selectedDate.month = this.currMonth();
+				}
+			}
+
+			if (year !== null ) {
+				this.selectedDate.year = year;
+			} else {	
+				if (this.selectedDate.year === null) {
+					this.selectedDate.year = this.currYear();
+				}
+			}
+		},
+		prewYear: function(){
+			this.buildCalendar(this.selectedDate.year-1, null);
+		},
+		nextYear: function(){
+			this.buildCalendar(this.selectedDate.year+1, null);
+		},
+		prewMonth: function(){
+			this.buildCalendar(null, this.selectedDate.month-1);
+		},
+		nextMonth: function(){
+			this.buildCalendar(null, this.selectedDate.month+1);
+		}
 	},
 	beforeMount(){
-		this.buildCalendar(2018, 8);
+		this.buildCalendar();
 	}
 });
