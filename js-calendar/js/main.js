6fad022318dca330af30fff0496bde0b921ee7cd
Max Skvortsov <mkmax1615@gmail.com>
2018-08-15 14:48:13 +0300

    Add files via upload

--- js-calendar/js/main.js
+++ js-calendar/js/main.js
@@ -29,20 +29,60 @@ let app = new Vue({
 		],
 		selectedDate: {
 			year: null,
-			month: null
-		}
+			month: null,
+			day: null
+		},
+		currEvents: [],
+		eventName: '',
 	},
 	methods: {
+		showEvents: function(day=null){
+			this.currEvents = [];
+			if(day !== null){
+				this.selectedDate.day = day;
+			}
+			this.getEvents();
+
+		},
+		addEvent: function(){
+			if (!this.eventName.match(/\w+/g)) {
+				alert("Enter event name");
+				return;
+			}
+			this.currEvents = [];
+			this.getEvents();
+
+			this.currEvents.push({'name': this.eventName});
+			localStorage.setItem(this.getEventsId(), JSON.stringify(this.currEvents));
+			this.eventName = '';
+		},
+		getEvents: function(){
+
+			if (localStorage.getItem(this.getEventsId())) {
+				let events = JSON.parse(localStorage.getItem(this.getEventsId()));
+				this.currEvents = events;
+			}	
+		},
+		removeEvent: function(i){
+			this.currEvents.splice(i, 1);
+			if(this.currEvents.length == 0){
+				localStorage.removeItem(this.getEventsId());
+			} else{
+				localStorage.setItem(this.getEventsId(), JSON.stringify(this.currEvents));
+			}
+		},
+		getEventsId: function(iday, imonth, iyear){
+			if(iday && imonth && iyear){
+				return ''+iday+imonth+iyear;
+			}
+			let day = this.selectedDate.day;
+			let month = this.selectedDate.month;
+			let year = this.selectedDate.year;
+			
+			return ''+day+month+year;
+		},
 		buildCalendar: function(yearInp=null, monthInp=null){
 			this.setSelectedDate(yearInp, monthInp);
-
-			//let currDate = new Date();
-			/*let currYear = new Date().getFullYear();
-			let currMonth = new Date().getMonth();
-			let currDay = new Date();
-			*/
-			console.log(this.selectedDate.year);
-			console.log(this.selectedDate.month);
 
 			let year = this.selectedDate.year;
 			let month = this.selectedDate.month;
@@ -51,17 +91,16 @@ let app = new Vue({
 			let firstMonthDay = this.firstMonthDay(year, month);
 			let colsTotal = Math.ceil((daysInMonth+firstMonthDay)/7)*7; 
 
-			console.log(`firstMonthDay:${firstMonthDay}`);
-			//console.log(`month:${month}`);
-			console.log(`daysInMonth: ${daysInMonth}`);
-			console.log(`colsTotal: ${colsTotal}`);
-			//console.log(`currWeekDay:${this.currWeekDay(year, month)}`);
 			let colIndex = 0;
 			let day = 1;
 			let week = 1;
 			this.monthArr = [];
 			this.monthArr[week] ={};
 			do{
+				isToday = false;
+				isPlanned = false;
+				isHoliday = false;
+				isAccess = false;
 				let currDayNum = colIndex%7;
 				//monthArr.push(week);
 			    if((colIndex < firstMonthDay) || (colIndex >= (firstMonthDay+daysInMonth))){
@@ -70,19 +109,29 @@ let app = new Vue({
 			    	//monthArr[week][colIndex%7] = {};
 			    } else {
 			    	className = '';
-			    	if(this.currYear() == year && 
-			    		this.currMonth() == month &&
-			    		this.currDay() == day){
-			    		className = 'today';
+			    	if (this.currYear() <= year){
+			    		if (true) {}
+			    		isAccess = true;
 			    	} 
+			    	if(this.currDay() == day &&
+			    		this.currMonth() == month &&
+			    		this.currYear() == year){
+			    		isToday = true;
+			    	}
+			    	if (localStorage.getItem(this.getEventsId(day, month, year))){
+			    		isPlanned = true;
+			    	}
 			    	else if (currDayNum == 6 || currDayNum == 5) {
-			    		className = 'holiday';
+			    		isHoliday = true;
 			    	}
 			    	this.monthArr[week][currDayNum] = {
 			    		'year': year, 
 			    		'month': month, 
-			    		'day': day, 
-			    		'class': className
+			    		'day': day,
+			    		'isToday': isToday,
+			    		'isHoliday': isHoliday,
+			    		'isPlanned': isPlanned,
+			    		'isAccess' : isAccess
 			    	};
 			    	day++
 			    }
@@ -101,14 +150,22 @@ let app = new Vue({
 		daysInMonth: function(year, month){
 			return new Date(year, month+1, 0).getDate();	
 		},
-		currDay: function(year, month){
-			return new Date(year, month).getDate();
+		currDay: function(){
+			return new Date().getDate();
 		},
 		currYear: function(){
 			return new Date().getFullYear();
 		},
 		currMonth: function(){
 			return new Date().getMonth();
+		},
+		dayOfWeek: function(){
+
+			let num = new Date(
+				this.selectedDate.year,
+			 	this.selectedDate.month,
+			  	this.selectedDate.day).getDay();
+			return this.weekDays[num];
 		},
 		firstMonthDay: function(year, month){
 
@@ -118,6 +175,7 @@ let app = new Vue({
 			}
 			return --res;
 		},
+		
 		setSelectedDate: function(year, month) {
 			if (month !== null) {
 				if (month > 11) {
@@ -141,6 +199,7 @@ let app = new Vue({
 					this.selectedDate.year = this.currYear();
 				}
 			}
+			this.selectedDate.day = null;
 		},
 		prewYear: function(){
 			this.buildCalendar(this.selectedDate.year-1, null);
